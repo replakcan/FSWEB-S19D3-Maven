ed2f0895aec582473e36b130c55cdfeec3ec994c
replakcan <alper.makcan@gmail.com>
2025-02-12 15:10:17 +0300

    spring boot security

---
+++ src/main/java/com/workintech/s19d2/controller/AccountController.java
@@ -0,0 +1,30 @@
+package com.workintech.s19d2.controller;
+
+import com.workintech.s19d2.entity.Account;
+import com.workintech.s19d2.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+
+    private AccountService accountService;
+
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping
+    public List<Account> findAll() {
+        return accountService.findAll();
+    }
+
+    @PostMapping
+    public Account save(@RequestBody Account account) {
+        return accountService.save(account);
+    }
+}
